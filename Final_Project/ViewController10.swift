cf825bcab710f3c848929034d999cc54206b726e
Sonali <sonalisharma58521@gmail.com>
2020-07-15 01:08:33 -0500

    Added a lot of view controllers and a few scroll views, completed the segues for them, labeled them, and the news view controller is ViewController21, or whatever the last numbered view controller is

---
+++ Final_Project/ViewController10.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController10.swift
+//  Final_Project
+//
+//  Created by Apple on 7/15/20.
+//  Copyright © 2020 Sonali Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController10: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
